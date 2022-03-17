13ac8b56ee87ed6a7c72e82afc5f7fe388a6746c
Daniel Conroy <dannyconroy23@hotmail.com>
2022-03-17 17:05:04 -0400

    commit tree cf9e97bac97d0d0af337c61eb1be99e8f2591ed3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+//"Guadalupe", "Ollie", "Aki"
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names) {
+  console.log(`Thank you, ${names} for the wonderful suprise gift!`);
+}
